1a0a6a0167f4be998a152db76c03f3a7b698de6f
QianShu99 <144203997+QianShu99@users.noreply.github.com>
2023-12-22 21:13:41 +0800

    Add files via upload

---
+++ 分.lua
@@ -0,0 +1,7 @@
+local TAIJI = loadstring(game:HttpGet('https://pastebin.com/raw/D3Q8RwS6'))()
+
+local A1 = library:CreateTab("支持游戏");
+
+A1:NewButton("兵工厂", function()
+loadstring(game:HttpGet("https://raw.github.com/QianShu99/TJJJ/main/%E5%85%B5.lua"))()
+end)
