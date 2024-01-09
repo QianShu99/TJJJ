b8d33cb2c20e30dfbbc59edbb322c542c6d4510b
QianShu99 <144203997+QianShu99@users.noreply.github.com>
2024-01-09 23:29:54 +0800

    Update 分.lua

--- 分.lua
+++ 分.lua
@@ -5,3 +5,7 @@ local A1 = library:CreateTab("支持游戏");
 A1:NewButton("兵工厂", function()
 loadstring(game:HttpGet("https://raw.github.com/QianShu99/TJJJ/main/%E5%85%B5.lua"))()
 end)
+
+A1:NewButton("刀刃球", function()
+loadstring(game:HttpGet("https://raw.github.com/QianShu99/TJJJ/main/%E5%88%80.lua"))()
+end)
